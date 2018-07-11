865719164bfd3de8c7c7da3bd654b7e8ac922a4e
Fabricio S. Paranhos <paranhos.s.f@gmail.com>
2018-07-11 13:21:20 -0300

    Modificação do gerenciador de arquivos.

--- src/FileManager.hh
+++ src/FileManager.hh
@@ -1,37 +1,16 @@
 #ifndef _FILE_MANAGER_HH_
 #define _FILE_MANAGER_HH_
 
-#include <fstream>
 #include <string>
 #include <vector>
 
-class FileManager {
-private:
-  std::string _prefix_path;
+// Verifica se arquivo existe
+bool file_exist(const std::string file);
 
-public:
+// Verifica se file é um diretório.
+bool file_is_dir(const std::string file);
 
-  enum STATUS { OK, F_NOT_FOUND, F_NOT_DIR };
-
-  // Construtor
-  FileManager(const std::string absolute_path);
-
-  // Mostra diretórios dentro do diretório.
-  std::vector<std::string> listDir(const std::string path) const;
-
-  // Retorna um Manager de um sub-diretório.
-  FileManager getDir(const std::string path) const;
-
-  // Retorna stream do arquivo para leitura.
-  void readFile(const std::string file, std::ifstream &f) const;
-
-  // Retorna stream de arquivo para escrita.
-  void writeFile(const std::string file, std::ofstream &f) const;
-
-  // Retorna caminho atual
-  std::string getPath() const;
-
-  static std::string errMsg(const STATUS s);
-};
+// Obtem diretórios dentro de outro diretório.
+std::vector<std::string> get_dir(const std::string dir);
 
 #endif  // _FILE_MANAGER_HH_
